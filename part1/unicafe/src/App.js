75fa1d28ee5b75493683685b656f360a9dc22bd3
gentilmente <egentilemontes@gmail.com>
2022-09-28 22:30:28 -0300

    1.7 unicafe step2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,13 +1,14 @@
 import { useState } from "react";
+
 const Header = ({ title }) => <h1>{title}</h1>;
 
 const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>{text}</button>
 );
 
-const Show = ({ text, counter }) => (
+const Show = ({ text, value }) => (
   <div>
-    {text}: {counter}
+    {text}: {value}
   </div>
 );
 
@@ -19,6 +20,10 @@ const App = () => {
   const increaseGood = () => setGood(good + 1);
   const increaseNeutral = () => setNeutral(neutral + 1);
   const increaseBad = () => setBad(bad + 1);
+  const all = good + neutral + bad;
+  const average = all ? (good - bad) / all : 0;
+  const positiveCalc = (good / all) * 100;
+  const positive = isNaN(positiveCalc) ? 0 : positiveCalc;
 
   return (
     <div>
@@ -27,9 +32,12 @@ const App = () => {
       <Button handleClick={increaseNeutral} text="neutral" />{" "}
       <Button handleClick={increaseBad} text="bad" />
       <Header title="Statistics:" />
-      <Show text="Good" counter={good} />
-      <Show text="Neutral" counter={neutral} />
-      <Show text="Bad" counter={bad} />
+      <Show text="Good" value={good} />
+      <Show text="Neutral" value={neutral} />
+      <Show text="Bad" value={bad} />
+      <Show text="All" value={all} />
+      <Show text="Average" value={average} />
+      <Show text="Positive" value={positive + "%"} />
     </div>
   );
 };
