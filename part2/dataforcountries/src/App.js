7e3042807790c201c4e0e321912ea3a9bd67142a
gentilmente <egentilemontes@gmail.com>
2022-10-03 21:04:01 -0300

    first 2.12* Data for countries, step1

--- part2/dataforcountries/src/App.js
+++ part2/dataforcountries/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
+const App = () => {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://restcountries.com/v3.1/all").then((response) => {
+      console.log("hola");
+      setCountries(response.data);
+      console.log("chau");
+    });
+  }, []);
+
+  const handleInputChange = (event) => {
+    console.log(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Cities</h1>
+      find countries <input onChange={handleInputChange} />
+      <ul>
+        {console.log("a", countries)}
+        {countries.length > 1
+          ? //countries.forEach((c) => console.log(c.name.common))
+            countries.map((c) => <li key={c.name.common}>{c.name.common}</li>)
+          : null}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
