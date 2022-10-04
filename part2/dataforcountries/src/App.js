adf98abcb504e6a7963bbfade2ff30bf4177c391
gentilmente <egentilemontes@gmail.com>
2022-10-03 22:58:22 -0300

    ok flag area capital

--- part2/dataforcountries/src/App.js
+++ part2/dataforcountries/src/App.js
@@ -3,6 +3,7 @@ import axios from "axios";
 
 const App = () => {
   const [countries, setCountries] = useState([]);
+  const [filtered, setFiltered] = useState([]);
 
   useEffect(() => {
     axios.get("https://restcountries.com/v3.1/all").then((response) => {
@@ -13,7 +14,14 @@ const App = () => {
   }, []);
 
   const handleInputChange = (event) => {
-    console.log(event.target.value);
+    //console.log(event.target.value);
+    //countries.forEach((c) => console.log(c.name.common));
+    const filteredo = countries.filter((c) =>
+      c.name.common.includes(event.target.value)
+    );
+    console.log(filteredo);
+    setFiltered([...filteredo]);
+    console.log("coso", filtered); //ojo, trae el contenido anterior al set
   };
 
   return (
@@ -21,11 +29,24 @@ const App = () => {
       <h1>Cities</h1>
       find countries <input onChange={handleInputChange} />
       <ul>
-        {console.log("a", countries)}
-        {countries.length > 1
-          ? //countries.forEach((c) => console.log(c.name.common))
-            countries.map((c) => <li key={c.name.common}>{c.name.common}</li>)
-          : null}
+        {/*console.log(countries)*/}
+        {filtered.length === 1 ? (
+          <div>
+            <h1> {filtered[0].flag} </h1>
+            <p>area: {filtered[0].area}</p>
+            <p>capital: {filtered[0].capital}</p>
+            <h3>Languages</h3>
+            {/*  <ul>
+              {filtered[0].languages.map((l) => (
+                <li>{l}</li>
+              ))}
+              </ul> */}
+          </div>
+        ) : filtered.length <= 10 ? (
+          filtered.map((c) => <li key={c.name.common}>{c.name.common}</li>)
+        ) : (
+          "Too many matches, specify another filter"
+        )}
       </ul>
     </div>
   );
