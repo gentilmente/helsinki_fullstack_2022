b6a06c61a48c26c2d70989ffccca8ac2a4d9457e
gentilmente <egentilemontes@gmail.com>
2022-10-15 19:25:53 -0300

    4.1 Blog list, step1

---
+++ part4/README.md
@@ -0,0 +1,2 @@
+# blog_list
+Exercises 4.1.-4.2. fullstack 2022 Helsinki
---
+++ part4/index.js
@@ -0,0 +1,48 @@
+require("dotenv").config();
+const http = require("http");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+});
+
+const Blog = mongoose.model("Blog", blogSchema);
+
+const url = process.env.MONGODB_URI;
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log("connected to MongoDB..");
+  })
+  .catch((error) => {
+    console.log("error connecting to MongoDB:", error.message);
+  });
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/blogs", (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs);
+  });
+});
+
+app.post("/api/blogs", (request, response) => {
+  const blog = new Blog(request.body);
+
+  blog.save().then((result) => {
+    response.status(201).json(result);
+  });
+});
+app.get("/", (req, res) => res.send("<h1>Hello backend!</h1>"));
+
+const PORT = 3003;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
