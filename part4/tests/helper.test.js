0034995f79d06ed63a6ccfb0a54dc39e45912643
gentilmente <egentilemontes@gmail.com>
2022-10-16 20:22:28 -0300

    4.4: helper functions and unit tests, step2

--- part4/tests/helper.test.js
+++ part4/tests/helper.test.js
@@ -1,8 +1,29 @@
 const listHelper = require('../utils/list_helper');
+const listWithOneBlog = require('./testMockData');
 
-test('dummy returns one', () => {
-  const blogs = [];
+const blogs = [
+  {
+    title: 'jlkh',
+    author: 'khg',
+    url: 'ñkj',
+    likes: 2,
+  },
+];
+// console.log(listWithOneBlog);
 
-  const result = listHelper.dummy(blogs);
-  expect(result).toBe(1);
+describe('total likes', () => {
+  test('dummy returns one', () => {
+    const result = listHelper.dummy(blogs);
+    expect(result).toBe(1);
+  });
+
+  test('when list has only one blog, equals the likes of that', () => {
+    const result = listHelper.totalLikes(blogs);
+    expect(result).toBe(blogs[0].likes);
+  });
+
+  test('totalLikes returns sum of all likes', () => {
+    const result = listHelper.totalLikes(listWithOneBlog);
+    expect(result).toBe(36);
+  });
 });
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,5 +1,6 @@
 const dummy = (blogs) => (blogs ? 1 : 0);
-
+const totalLikes = (blogs) => blogs.reduce((acc, curr) => acc + curr.likes, 0);
 module.exports = {
   dummy,
+  totalLikes,
 };
