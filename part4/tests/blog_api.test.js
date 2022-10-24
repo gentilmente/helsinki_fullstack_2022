bdb399e54f6453063990b290cc49555b8ffa6c0d
gentilmente <egentilemontes@gmail.com>
2022-10-24 14:19:12 -0300

    4.9*: Blog list tests, step2

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -6,15 +6,15 @@ const app = require("../app");
 const api = supertest(app);
 const Blog = require("../models/blog");
 
-jest.setTimeout(15000);
+jest.setTimeout(12000);
 
-beforeEach(async () => {
+/* beforeEach(async () => {
   await mongoose.connection.dropCollection("blogs");
 
   const blogObjects = helper.initialBlogs.map((blog) => new Blog(blog));
   const promiseArray = blogObjects.map((blog) => blog.save());
   await Promise.all(promiseArray);
-});
+}); */
 
 test("blogs are returned as json", async () => {
   await api
@@ -26,6 +26,11 @@ test("blogs are returned as json", async () => {
 test("all blogs are returned", async () => {
   const response = await api.get("/api/blogs");
   expect(response.body).toHaveLength(helper.initialBlogs.length);
+});
+
+test("check if prop id exists", async () => {
+  const res = await helper.blogsInDb();
+  expect(res[0].id).toBeDefined();
 });
 
 afterAll(() => {
