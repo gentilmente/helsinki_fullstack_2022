ae744c9accd2269a09d12a406c96f52d76e80b87
gentilmente <egentilemontes@gmail.com>
2022-10-24 11:08:21 -0300

    4.8: Blog list tests, step1

--- part4/utils/config.js
+++ part4/utils/config.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-undef */
 require("dotenv").config();
 
-const PORT = process.env.PORT;
-const MONGODB_URI = process.env.MONGODB_URI;
+const { PORT } = process.env;
+const { MONGODB_URI } = process.env;
 
 module.exports = {
   MONGODB_URI,
